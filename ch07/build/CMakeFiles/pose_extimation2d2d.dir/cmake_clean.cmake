1266f11c71d093849494b1b4bfe086542a327a1d
Yong <wangyg1025@163.com>
2018-10-21 15:58:26 +0800

    update pose_estimation2d2d

---
+++ ch07/build/CMakeFiles/pose_extimation2d2d.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pose_extimation2d2d.dir/pose_extimation2d2d.cpp.o"
+  "pose_extimation2d2d.pdb"
+  "pose_extimation2d2d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose_extimation2d2d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
